9cdbf4100a325a06ea0b77a5ea228781cc022cfe
Anton Vlasenko <antares.spica@gmail.com>
2012-10-23 01:13:04 +0300

    Initial commit - basic project structure

---
+++ JulieNotes/main.py
@@ -0,0 +1,18 @@
+from JulieNotes.design.output import Ui_MainWindow
+from PySide import QtCore, QtGui
+
+import sys
+
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = MainWindow()
+    myapp.show()
+    sys.exit(app.exec_())
+
