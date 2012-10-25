f4de451575d2db210525f258b3806184d78ca3eb
Anton Vlasenko <antares.spica@gmail.com>
2012-10-25 23:55:49 +0300

    Save / Load, Save_As, tableview

--- JulieNotes/main.py
+++ JulieNotes/main.py
@@ -1,5 +1,5 @@
-from PySide.QtCore import QUrl
-from PySide.QtGui import QTabBar
+from PySide.QtCore import QUrl, QAbstractTableModel, Qt
+from PySide.QtGui import QTabBar, QFileDialog
 from JulieNotes.design.output import Ui_MainWindow
 from PySide import QtCore, QtGui
 
@@ -8,11 +8,25 @@ from JulieNotes.core.restructedText import reST_to_html
 
 class MainWindow(QtGui.QMainWindow):
 
+    __last_key = None
+    __opened_file = ''
+
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.__handle_events()
+
+        # keep header/title list separate
+        header = ['Test', 'Test']
+        # a list of (name, age, weight) tuples
+        data_list = [
+            ('Test Test', '362'),
+            ('Test Test1', '212'),
+        ]
+
+        table_model = MyTableModel(self, data_list, header)
+        self.ui.TableView.setModel(table_model)
 
         # Debug:
         with open('../EXAMPLE.rst', 'r') as f:
@@ -27,11 +41,41 @@ class MainWindow(QtGui.QMainWindow):
         self.ui.action_Quit.triggered.connect(QtCore.QCoreApplication.instance().quit)
         # Open / Save
         self.ui.action_Open.triggered.connect(self._load_from_file)
-        self.ui.action_Save.triggered.connect(self._save_to_file)
+        self.ui.action_Save.triggered.connect(self._save_current)
         self.ui.action_Save_As.triggered.connect(self._save_to_file)
         self.ui.actionAbout.triggered.connect(self._show_about)
         # Tabs change handle
         self.ui.View.findChild(QTabBar).currentChanged.connect(self._tab_pressed)
+
+    def __open_file(self, path):
+        try:
+            with open(path, 'r') as f:
+                text = f.read()
+        except Exception as e:
+            print (e)
+        else:
+            try:
+                self.ui.textEdit.setText(text)
+            except Exception as e:
+                print (e)
+            else:
+                self.__opened_file = path
+
+    def __save_to_file(self, path=None):
+        if path is None:
+            path = self.__opened_file
+        if not path:
+            return # save nothing or any other case
+
+        text = self.ui.textEdit.toPlainText()
+
+        try:
+            with open(path, 'w') as f:
+                f.write(text)
+        except Exception as e:
+            print (e)
+        else:
+            self.__opened_file = path
 
     def _render_text(self, text):
         render = reST_to_html(text)
@@ -43,15 +87,43 @@ class MainWindow(QtGui.QMainWindow):
         self.ui.qwebview.load(QUrl("./core/template/rendered_file.html"))
         self.ui.qwebview.show()
 
+    def keyPressEvent(self, event):
+
+        key = event.key()
+
+        if key == 16777249:
+            self.__last_key = key
+        elif key == 83 and self.__last_key == 16777249:
+            self._save_current()
+        else:
+            QtGui.QWidget.keyPressEvent(self, event)
+
+        """ This code does not work, but should:
+        if key == QtCore.Qt.Key_Save:
+            self._save_to_file()
+        elif key == QtCore.Qt.Key_Open:
+            self._load_from_file()
+        else:
+            QtGui.QWidget.keyPressEvent(self, event)
+        """
+
     def _tab_pressed(self):
         text = self.ui.textEdit.toPlainText()
         self.show_in_browser(text)
 
+    def _save_current(self):
+        self.__save_to_file()
+
     def _save_to_file(self):
-        print ('Trying to save')
+        if not self.__opened_file:
+            return
+        filepath = QFileDialog.getOpenFileName(self, "New file name", '~', "All files (*.*)")
+        self.__save_to_file(path=filepath[0])
 
     def _load_from_file(self):
-        print ('Trying to load')
+        filepath = QFileDialog.getOpenFileName(self, "Open rst file", '~', "All files (*.*)")
+        self.__open_file(filepath[0])
+        self._tab_pressed()
 
     def _show_about(self):
         print ('Trying to show about')
@@ -59,6 +131,26 @@ class MainWindow(QtGui.QMainWindow):
     def show(self, *args, **kwargs):
         self._tab_pressed()
         super().show()
+
+
+class MyTableModel(QAbstractTableModel):
+    def __init__(self, parent, mylist, header, *args):
+        QAbstractTableModel.__init__(self, parent, *args)
+        self.mylist = mylist
+        self.header = header
+
+    def rowCount(self, parent):
+        return len(self.mylist)
+
+    def columnCount(self, parent):
+        return len(self.mylist[0])
+
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+        elif role != Qt.DisplayRole:
+            return None
+        return self.mylist[index.row()][index.column()]
 
 
 if __name__ == "__main__":
