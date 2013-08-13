0943f6914d288f976ec4878acb70b63fc0df4017
Anton Vlasenko <antares.spica@gmail.com>
2013-08-13 14:08:24 +0300

    Reformatting according to PEP8

--- JulieNotes/restructedText.py
+++ JulieNotes/restructedText.py
@@ -1,18 +1,23 @@
 from docutils import core
-from docutils.writers.html4css1 import Writer,HTMLTranslator
+from docutils.writers.html4css1 import Writer, HTMLTranslator
 
-class HTMLFragmentTranslator( HTMLTranslator ):
-    def __init__( self, document ):
-        HTMLTranslator.__init__( self, document )
+
+class HTMLFragmentTranslator(HTMLTranslator):
+    
+    def __init__(self, document):
+        HTMLTranslator.__init__(self, document)
         self.head_prefix = ['','','','','']
         self.body_prefix = []
         self.body_suffix = []
         self.stylesheet = []
+        
     def astext(self):
         return ''.join(self.body)
+
 
 html_fragment_writer = Writer()
 html_fragment_writer.translator_class = HTMLFragmentTranslator
 
-def reST_to_html( s ):
-    return core.publish_string( s, writer = html_fragment_writer )
+
+def reST_to_html(s):
+    return core.publish_string(s, writer = html_fragment_writer)
